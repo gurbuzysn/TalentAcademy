6e503367913d2ec208d2ec539bcf2515019a058e
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-05-03 21:26:00 +0300

    StudentController GetAll Action Created

---
+++ Presentation/TalentAcademy.WebAPI/Controllers/StudentsController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TalentAcademy.Application.Features.Queries.Student.GetAllStudent;
+
+namespace TalentAcademy.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public StudentsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var allStudents = await _mediator.Send(new GetAllStudentsQueryRequest());
+
+            return Ok(allStudents);
+        }
+    }
+}
--- Presentation/TalentAcademy.WebAPI/Program.cs
+++ Presentation/TalentAcademy.WebAPI/Program.cs
@@ -1,10 +1,24 @@
 using Microsoft.EntityFrameworkCore;
+using TalentAcademy.Application;
+using TalentAcademy.Application.Features.Queries.Student.GetByIdStudent;
+using TalentAcademy.Application.Features.Queries.Students.GetAllStudent;
+using TalentAcademy.Application.Repositories;
 using TalentAcademy.Persistence.Context;
+using TalentAcademy.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<TalentAcademyDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
+builder.Services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
+
+builder.Services.AddScoped<GetAllStudentsQueryHandler>();
+builder.Services.AddScoped<GetStudentByIdQueryHandler>();
+
+
+
+builder.Services.AddApplicationServices();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
