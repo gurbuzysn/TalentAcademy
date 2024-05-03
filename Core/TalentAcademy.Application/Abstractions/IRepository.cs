c054145bef8d5243c139a9d33d07e6b19c172985
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-05-03 10:39:50 +0300

    IRepository Interface Created

---
+++ Core/TalentAcademy.Application/Abstractions/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalentAcademy.Application.Abstractions
+{
+    public interface IRepository<T> where T : class, new()
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(Guid id);
+        Task<T> GetByFilterAsync(Expression<Func<T, bool>> filter);
+        Task<T> CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
