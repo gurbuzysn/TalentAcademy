47bb93ac2339310a5bb31bd9d3ca28887d9bbd9c
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-05-03 11:16:29 +0300

    ServiceRegistrations

---
+++ Core/TalentAcademy.Application/ServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalentAcademy.Application
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationServices()
+        {
+
+        }
+    }
+}
---
+++ Infrastructure/TalentAcademy.Persistence/ServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalentAcademy.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices()
+        {
+
+        }
+    }
+}
