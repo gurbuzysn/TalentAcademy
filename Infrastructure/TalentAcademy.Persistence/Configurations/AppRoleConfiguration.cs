d82d3a21c281b0c542026268ec6d1d7a0d4719b9
yasinn gürbüz <yasingurbuz1@hotmail.com>
2024-05-06 00:54:46 +0300

    .

---
+++ Infrastructure/TalentAcademy.Persistence/Configurations/AppRoleConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalentAcademy.Domain.Entities.Identitiy;
+
+namespace TalentAcademy.Persistence.Configurations
+{
+    public class AppRoleConfiguration : IEntityTypeConfiguration<AppRole>
+    {
+        public void Configure(EntityTypeBuilder<AppRole> builder)
+        {
+            builder.Property(x => x.Name).IsRequired();
+
+            AppRole adminRole = new AppRole()
+            {
+                Id = new Guid(),
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            };
+
+            builder.HasData(adminRole);
+        }
+    }
+}
