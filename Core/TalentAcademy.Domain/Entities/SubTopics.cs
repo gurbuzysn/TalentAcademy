3d0c5401a303875bdf77373ff9f62ea5caf15b06
Yasin GÜRBÜZ <yasingurbuz@isyazilim.com.tr>
2024-10-01 12:15:20 +0300

    SubTopics

---
+++ Core/TalentAcademy.Domain/Entities/SubTopics.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalentAcademy.Domain.Entities
+{
+    public class SubTopics
+    {
+        public Guid Id { get; set; }
+
+    }
+}
